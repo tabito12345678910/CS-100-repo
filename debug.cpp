a84bc018b004208b88b3d0f34ba3ac7bd29bd06d
tabito12345678910 <tabito@gmail.com>
2024-01-31 15:17:29 -0800

    GDB

---
+++ debug.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include <cstring> 
+
+using namespace std; 
+
+int main() {
+    char* input = NULL; 
+    int i = 42; 
+    cin.getline(input, 50);
+    for (i = strlen(input); i >= 0; i--) { 
+        cout << input[i];
+    }
+    cout << endl; 
+    return 0;
+}
